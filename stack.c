c23b5e88a1bd43b7fb10386f0fb6c74e157dcdcb
Fábio Nicolau <misterjs247000@gmail.com>
2025-08-07 20:13:31 +0100

    wip

--- stack.c
+++ stack.c
@@ -6,7 +6,7 @@
 /*   By: fnicolau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 13:41:58 by fnicolau          #+#    #+#             */
-/*   Updated: 2025/08/07 19:24:49 by fnicolau         ###   ########.fr       */
+/*   Updated: 2025/08/07 20:13:10 by fnicolau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,9 +53,12 @@ int	peek(t_node *head)
 	return (head->data);
 }
 
-void	disp_stack(t_node *node)
+void	disp_stack(t_node *node, char *title)
 {
-	printf("========= stack status ==========\n");
+	if (title)
+		printf("========= %s ==========\n", title);
+	else
+		printf("========= stack status ==========\n");
 	if (is_empty(node))
 		printf("stack is empty\n");
 	else
@@ -84,34 +87,56 @@ void	free_stack(t_node **head)
 	}
 	*head = NULL;
 }
+// void	merge_stack(t_node **stack)
+// {
+// 	size_t	middle;
+
+// 	if (start == end)
+// 		return ;
+// 	middle = start + (end - start) / 2;
+// 	merge_stack(stack, start, middle);
+// 	merge_stack(stack, middle + 1, end);
+// 	merge(stack, start, middle, end);
+// }
 
 int	main(void)
 {
 	t_node	*head;
 
 	head = NULL;
+	// testing basic operations within a stack
 	push(NULL, 2);
-	disp_stack(head);
+	disp_stack(head, NULL);
 	push(&head, 3);
-	disp_stack(head);
+	disp_stack(head, NULL);
 	push(&head, 5);
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("pop: %d\n", pop(&head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("peek: %d\n", peek(head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	push(&head, 30);
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("pop: %d\n", pop(&head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	push(&head, 77);
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("peek: %d\n", peek(head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("pop: %d\n", pop(&head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	printf("peek: %d\n", peek(head));
-	disp_stack(head);
+	disp_stack(head, NULL);
 	free_stack(&head);
+
+	// sorting a stack
+	push(&head, 3);
+	push(&head, 5);
+	push(&head, 30);
+	push(&head, 77);
+
+	disp_stack(head, "(before sorting)");
+	// merge_stack(&head);
+	disp_stack(head, "(after sorting)");
 	return (0);
 }
